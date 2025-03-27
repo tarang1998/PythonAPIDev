3a72ed6b5e32359f36b310ad366134ba968b3633
Tarang <tarangnair98@gmail.com>
2025-03-27 01:21:33 -0400

    Handling the CORS issue

--- social_app/main.py
+++ social_app/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routes import post, auth, user, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # SqlAlchemy Creates the tables in the DB for the models defined 
@@ -15,6 +16,18 @@ from .routes import post, auth, user, vote
 
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
